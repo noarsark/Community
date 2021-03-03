71c06d9a81cbf060f78977fd37046d304d3f9166
noarsark <noarsark817@163.com>
2021-03-03 23:08:40 +0800

    Community2.1 20210303

---
+++ src/main/java/com/nowcoder/community/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityConstant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class LoginController implements CommunityConstant {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(path = "/register", method = RequestMethod.GET)
+    public String getRegisterPage() {
+        return "/site/register";
+    }
+}
---
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -0,0 +1,26 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.DiscussPost;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface DiscussPostMapper {
+
+    List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit);
+
+    /**
+     * 查询userId发表的帖子数量
+     * Param注解用于给参数取别名
+     * 如果只用一个参数, 并且在<if>里使用， 则必须加别名
+     * @param userId
+     * @return
+     */
+    int selectDiscussPostRows(@Param("userId") int userId);
+
+    
+}
