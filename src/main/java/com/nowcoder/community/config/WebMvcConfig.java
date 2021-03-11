cddf4ff06836eaaa0a948e6a3e7c9072c22586ea
noarsark <noarsark817@163.com>
2021-03-11 22:05:06 +0800

    login Completion3.10

---
+++ src/main/java/com/nowcoder/community/config/WebMvcConfig.java
@@ -0,0 +1,9 @@
+package com.nowcoder.community.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+}
---
+++ src/main/java/com/nowcoder/community/controller/interceptor/AlphaInterceptor.java
@@ -0,0 +1,56 @@
+package com.nowcoder.community.controller.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@Component
+public class AlphaInterceptor implements HandlerInterceptor {
+
+    private static final Logger logger = LoggerFactory.getLogger(AlphaInterceptor.class);
+    /**
+     * 在Controller请求之前执行
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.debug("preHandle: " + handler.toString());
+        return true;
+    }
+
+    /**
+     * 在Controller之后执行
+     * @param request
+     * @param response
+     * @param handler
+     * @param modelAndView
+     * @throws Exception
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        logger.debug("postHandler: " + handler.toString());
+    }
+
+    /**
+     * 在模板引擎执行完之后执行, 最后执行
+     * @param request
+     * @param response
+     * @param handler
+     * @param ex
+     * @throws Exception
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        logger.debug("afterCompletion: " + handler.toString());
+    }
+}
