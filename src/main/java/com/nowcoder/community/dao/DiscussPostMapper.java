8e13893d2f045c8d5db48bbf93fa16b08dc469ba
noarsark <noarsark817@163.com>
2021-04-09 19:14:21 +0800

    3.1ok 3.2not ok

--- src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -15,12 +15,13 @@ public interface DiscussPostMapper {
 
     /**
      * 查询userId发表的帖子数量
-     * Param注解用于给参数取别名
+     * @ Param注解用于给参数取别名
      * 如果只用一个参数, 并且在<if>里使用， 则必须加别名
      * @param userId
      * @return
      */
     int selectDiscussPostRows(@Param("userId") int userId);
 
-    
+    int insertDisscussPost(DiscussPost discussPost);
+
 }
