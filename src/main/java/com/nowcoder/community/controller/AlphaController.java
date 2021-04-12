089fcdfec3934279e62744c5e4dd4fc88a7cb761
noarsark <noarsark817@163.com>
2021-04-12 22:26:02 +0800

    3.2

--- src/main/java/com/nowcoder/community/controller/AlphaController.java
+++ src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -172,6 +172,11 @@ public class AlphaController {
         return "get cookie";
     }
 
-
-
+    @RequestMapping(path = "/ajax", method = RequestMethod.POST)
+    @ResponseBody
+    public String testAjax(String name, int age) {
+        System.out.println(name);
+        System.out.println(age);
+        return CommunityUtil.getJSONString(0, "操作成功!");
+    }
 }
