ea9b7ccbed427bc8a983468b07593f7a29ddf960
noarsark <noarsark817@163.com>
2022-01-13 08:46:48 +0800

    7.9 image upload qiniucloud

--- src/main/java/com/nowcoder/community/controller/ShareController.java
+++ src/main/java/com/nowcoder/community/controller/ShareController.java
@@ -41,6 +41,9 @@ public class ShareController implements CommunityConstant {
     @Value("${wk.image.storage}")
     private String wkImageStorage;
 
+    @Value("${qiniu.bucket.share.url}")
+    private String shareBucketUrl;
+
     @RequestMapping(path = "/share", method = RequestMethod.GET)
     @ResponseBody
     public String share(String htmlUrl) {
@@ -57,11 +60,12 @@ public class ShareController implements CommunityConstant {
 
         // 返回访问路径
         Map<String, Object> map = new HashMap<>();
-        map.put("shareUrl", domain + contextPath + "/share/image/" + fileName);
-
+        // map.put("shareUrl", domain + contextPath + "/share/image/" + fileName);
+        map.put("shareUrl", shareBucketUrl + "/" + fileName);
         return CommunityUtil.getJSONString(0, null, map);
     }
 
+    // 废弃
     @RequestMapping(path = "/share/image/{fileName}", method = RequestMethod.GET)
     public void getShareImage(@PathVariable("fileName") String fileName, HttpServletResponse response) {
         if (StringUtils.isBlank(fileName)) {
